873f0d4a0405b73e20ed8edc66bd41d8bc92b204
Duc Le <duc.le@soundcloud.com>
2023-11-02 22:43:50 +0100

    chore: debug

--- app/handler/echo.go
+++ app/handler/echo.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/codecrafters-io/http-server-starter-go/app/port"
 	"github.com/codecrafters-io/http-server-starter-go/app/response"
 	"github.com/codecrafters-io/http-server-starter-go/app/router"
@@ -12,6 +14,7 @@ type Echo struct {
 
 func (e *Echo) Handle(r port.Request) (port.Response, error) {
 	match := e.matcher.FindMatch(r.Path())
+	fmt.Println(match)
 	response := response.NewTextPlainResponse()
 	response.StatusCode(200).Body([]byte(match))
 	return response, nil
